539d44a35d5ec95326fb30c90578d6e0e9fb95d5
nosdeedson <josemariazumira@gmail.com>
2019-12-15 13:33:16 -0300

    ainda sobre modulos

---
+++ node/pastaA/pastaB/acessarModulo.js
@@ -0,0 +1,7 @@
+const indoModuloA = require('../../moduloA')
+console.log(indoModuloA.ateLogo, indoModuloA.bemVindo, indoModuloA.ola)
+
+// we should respect the name of the files, we must respect the uppercase loewrcase
+
+const indoModuloB = require('../../moduloB')
+console.log(indoModuloB.bomDia, indoModuloB.boaNoite())
