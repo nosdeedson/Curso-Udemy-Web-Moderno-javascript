efe902d2a0943d0711fd045b28eb29020b52d728
nosdeedson <josemariazumira@gmail.com>
2020-01-04 10:53:57 -0300

    aula_ajax

--- ajax/server.js
+++ ajax/server.js
@@ -1,13 +1,34 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+const multer = require('multer')
 
 const app = express()
 app.use(express.static('.'))
 
-app.use(bodyParser.urlencoded({extended: true}))// antes de bodyParser pode ver o nome da requisição como abaixo
-//app.use('/login',bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-app.get('/teste', (requisicao, resposta) => resposta.send('OK') )
+//app.get('/teste', (requisicao, resposta) => resposta.send('OK') )
+//como criar uma nova página o que vai ser madando é o ok
 
+const storage = multer.diskStorage({
+    destination: function(requisicao, file, callback){
+        callback(null, './upload')
+    },
+    filename: function(requisicao,file, callback){
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({storage}).single('arquivo')
+
+app.post('/upload', (req, resp) => {
+    upload(req, resp, err =>{
+        if(err){
+            return resp.end('OCORREU UM ERRO')
+        }
+        resp.end('CONCLUÍDO COM SUCESSO')
+    })
+})
 app.listen( 8080, () => console.log( " servidor rodando"))
+
