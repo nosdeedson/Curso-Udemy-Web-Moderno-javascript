104f99af0052985b994dcfd42a5b6e95a2b49627
nosdeedson <josemariazumira@gmail.com>
2020-01-14 22:49:05 -0300

    gulp_rever

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -1,9 +1,5 @@
 const gulp = require('gulp')
 
- gulp.task('default', ()=>{
-    gulp.emit('copiar')
-})
-
-gulp.task('copiar', ()=>{
-    gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt']).pipe(gulp.dest('pastaB'))
+gulp.task('default', ()=>{
+    console.log("ok")
 })
