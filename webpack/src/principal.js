51405b63e5f12bf7cd65249b6588361a5dbab6f5
nosdeedson <josemariazumira@gmail.com>
2020-01-21 22:05:32 -0300

    aula_21_01

--- webpack/src/principal.js
+++ webpack/src/principal.js
@@ -1,5 +1,5 @@
 import Pessoa from './pessoa' 
-import './modulos/moduloA'
+import './assets'
 const atendente = new Pessoa
 
 console.log(atendente.cumprimentar())
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,10 +1,40 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
+const UgliFyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugins= require('optimize-css-assets-webpack-plugin')
+
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development':'production',
     entry: './src/principal.js',
     output:{
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    optimization:{
+        minimizer:[
+            new UgliFyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugins({})
+        ]
+    },
+    plugins:[
+        new miniCssExtractPlugin({
+            filename: 'estilo.css'
+        })
+    ],
+    module:{
+        rules:[{
+            test: /\.s?[ac]ss$/,
+            use:[
+                miniCssExtractPlugin.loader,
+                //'style-loader', // adiciona CSS a DOM injetando a tag <style>
+                'css-loader', // interpreta @import, url()...
+                'sass-loader'
+            ]
+        }]
     }
 }
